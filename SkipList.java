c40ecbcfa3fff7f5157469e59f5efa9ce7dd36e0
Simon David Pratt <me@SimonDavidPratt.com>
2011-04-29 19:29:03 -0400

    Initial commit

---
+++ SkipList.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+* Skiplist                                                                    *
+*                                                                             *
+* View README file for information about this project.                        *
+* View LICENSE file for license information.                                  *
+******************************************************************************/
+
+import java.util.*;
+
+public class SkipList implements List {
+}
